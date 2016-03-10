e02f70c08c15de31c4e4d71552b1c85bad168bdc
Jieao Zhu <zhujieao@gmail.com>
2016-03-09 21:01:05 -0500

    121, one must sell a stock after buy it.

---
+++ _121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1) return 0;
+        int curMin = prices[0];
+        int profit = 0;
+        for(int i = 1; i < prices.length; i++){
+            curMin = Math.min(curMin, prices[i]);
+            profit = Math.max(profit, prices[i] - curMin);
+        }
+        return profit;
+    }
+}

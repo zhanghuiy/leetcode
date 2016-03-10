7b9c1813d60c1dfa82af1d9592f962ecf1573058
Jieao Zhu <zhujieao@gmail.com>
2016-03-09 23:59:37 -0500

    124, better code style

--- _124_Binary_Tree_Maximum_Path_Sum.java
+++ _124_Binary_Tree_Maximum_Path_Sum.java
@@ -11,8 +11,15 @@ public class Solution {
     private int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         if(root == null) return 0;
-        maxPathSumHelper(root);
+        maxPathSumHelper_better_style(root);
         return max;
+    }
+    private int maxPathSumHelper_better_style(TreeNode root){
+        if(root == null) return 0;
+        int leftMax = Math.max(maxPathSumHelper(root.left),0);
+        int rightMax = Math.max(maxPathSumHelper(root.right),0);
+        max = Math.max(leftMax+rightMax+root.val, max);
+        return Math.max(leftMax,rightMax)+root.val;
     }
     private int maxPathSumHelper(TreeNode root){
         if(root == null) return 0;
